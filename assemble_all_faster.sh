30493e5e0a16ab3224ec18117f066115aa836a65
Jan Sarenik <jajomojo@gmail.com>
2020-03-17 11:59:39 +0100

    Fix assembly for C and C++ examples

--- assemble_all_faster.sh
+++ assemble_all_faster.sh
@@ -53,31 +53,31 @@ echo
 echo "C/C++ Integration and Mixing, examples/hello_world_c1..."
 cd hello_world_c1
 fasm -m 524288 ht.asm
-gcc -nostdlib -o hello hello.c ht.o
+gcc -no-pie -nostdlib -o hello hello.c ht.o
 cd ..
 cd hello_world_c2
 echo
 echo "C/C++ Integration and Mixing, examples/hello_world_c2..."
 fasm -m 524288 ht.asm
-gcc -nostdlib -o hello hello.c ht.o
+gcc -no-pie -nostdlib -o hello hello.c ht.o
 cd ..
 echo
 echo "C/C++ Integration and Mixing, examples/simplechat_c++..."
 cd simplechat_c++
 fasm -m 524288 ht.asm
-g++ -std=c++11 -o simplechat simplechat.cpp ht.o
+g++ -no-pie -std=c++11 -o simplechat simplechat.cpp ht.o
 cd ..
 echo
 echo "C/C++ Integration and Mixing, examples/simplechat_ssh_auth_c++..."
 cd simplechat_ssh_auth_c++
 fasm -m 524288 ht.asm
-g++ -std=c++11 -o simplechat_ssh simplechat_ssh.cpp ht.o
+g++ -no-pie -std=c++11 -o simplechat_ssh simplechat_ssh.cpp ht.o
 cd ..
 echo
 echo "C/C++ Integration and Mixing, examples/simplechat_ssh_c++..."
 cd simplechat_ssh_c++
 fasm -m 524288 ht.asm
-g++ -std=c++11 -o simplechat_ssh simplechat_ssh.cpp ht.o
+g++ -no-pie -std=c++11 -o simplechat_ssh simplechat_ssh.cpp ht.o
 cd ..
 echo
 echo "Done."
