679a3e19585b126db801211a1fcaacb9083b277b
Jan Sarenik <jajomojo@gmail.com>
2022-08-29 10:08:57 +0200

    chroot.sh: Add dsa hostkey

--- chroot.sh
+++ chroot.sh
@@ -6,12 +6,16 @@ BIN=${1:-"${CURDIR##*/}"}
 CHROOT=$CURDIR/chroot-$BIN
 
 genkey() {
-  ssh-keygen -t rsa -m PEM -N '' -f $CHROOT/etc/ssh/ssh_host_rsa_key
+  type=${1:-"dsa"}
+  F=$CHROOT/etc/ssh/ssh_host_${type}_key
+  test -r "$F" && return 0
+  ssh-keygen -t "$type" -m PEM -N '' -f $CHROOT/etc/ssh/ssh_host_${type}_key
 }
 
 mkdir -p $CHROOT/etc/ssh $CHROOT/proc
 cp $BIN $CHROOT
-test -r $CHROOT/etc/ssh/ssh_host_rsa_key || genkey
+genkey dsa
+genkey rsa
 exec unshare -U -m -p --fork -r --propagation slave sh -xc "
   mount -t proc proc $CHROOT/proc
   chroot $CHROOT /$BIN
